f071060a1fe2856ef86361deb46241346f33cf2a
Desmond Ho <desmond@humn.ai>
2022-01-19 17:06:29 +0000

    fix-remove-root-access-kms-policy

--- kms.tf
+++ kms.tf
@@ -10,17 +10,6 @@ resource "aws_kms_key" "default" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Sid": "Enable IAM User Permissions",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": [
-          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        ]
-      },
-      "Action": "kms:*",
-      "Resource": "*"
-    },
-    {
       "Sid": "Allow use of the key",
       "Effect": "Allow",
       "Principal": {
